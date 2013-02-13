b92c84e3de7713c5ac7aa42e849b79dbc7a0f120
Tyler Madouse <tjm5317@psu.edu>
2013-02-13 00:22:33 -0500

    updated Manager and StoreTest
    Still don't have a method to check the total sales from the music store.
    Also, I'm not sure if I'm checking the inventory correctly. I tried to
    add another movie and then query the inventory but got back a strange
    result. It looks like a memory address and not the name of the actual
    movie/movies in the inventory.

--- Store/src/store/Manager.java
+++ Store/src/store/Manager.java
@@ -1,41 +1,60 @@
 
 
-public class Manager extends Customer{
-	
-	public Manager()
+public class Manager extends Customer
+{
+    //Constructor calls super class to default construct manager
+    public Manager()
     {
-		super();
+        super();
     }
-	public Manager(int ID,
+    public Manager(int ID,
              String name,
              String password,
              double balance,
              String shoppingCart,
              String history)
     {
-		super(ID,name,password,balance,shoppingCart,history);
+        super(ID,name,password,balance,shoppingCart,history);
     }
-	
-	public void addMedia(String mediaObj)
-	{
-		
-	}
-	
-	public void deleteMedia(String mediaObj)
-	{
-		
-	}
-	
-	@Override
+    
+    //Adds media to store
+    //Takes in a Media object: mediaObj
+    //and the type of media as a String
+    public void addMedia(Media mediaObj, String type)
+    {
+           DBIO.add(mediaObj, type);
+    }
+    //Removes media from store
+    //Takes in Media object: mediaObj
+    //and type of media as a String
+    public void deleteMedia(Media mediaObj, String type)
+    {
+           DBIO.remove(mediaObj,type);
+    }
+    
+    // Retrieve all information of any customer
+    public static String getcustInfo(Customer cust)
+    {
+        
+        return cust.toString();         
+    }
+    //Takes in media object
+    //will return the number sold
+    public static int getnumSold(Media mediaObj) 
+    {
+        return mediaObj.numSold;
+    }
+
+    @Override
     public String toString()
     {
         return String.format("::MANAGER INFORMATION::\n"
-                    + "    Identification #: %20s \n"
-                    + "    The Manager:   	%20s \n"
-                    + "    Password:   		%20s \n"
-                    + "    Balance:        %20f \n"
-                    + "    Shopping Cart:   %20s  \n"
-                    + "    History:           %20s \n",
+                    + " Identification #: %20s \n"
+                    + " The Manager: %20s \n"
+                    + " Password: %20s \n"
+                    + " Balance: %20f \n"
+                    + " Shopping Cart: %20s \n"
+                    + " History: %20s \n",
                     this.ID, this.name, this.password,
                     this.balance, this.shoppingCart, this.history);
         
--- Store/src/store/StoreTest.java
+++ Store/src/store/StoreTest.java
@@ -1,28 +1,38 @@
-
 public class StoreTest
 {
-	public static void main(String[] args) 
-	{ 
-		
-		Audiobook aud1 = new Audiobook("Josh", "2013", 120, "horror", 0, 30, 0,0);
-		Movie mov1 = new Movie("Josh", "2013", 120, "horror", 0, 30, 0,0);
-		
-		Audiobook [] aInv = new Audiobook[1];
-		Movie [] mInv = new Movie[1];
-		mInv[0] = mov1;
-		
-		DBIO.setMovieInventory(mInv);
-		DBIO.setBookInventory(aInv);
-		
-		Customer cust1 = new Customer(1,"Josh","pw",25.00,"Transformers","Spider Man 3");
-		Customer cust2 = new Customer(2,"Jared","pw1",35.00,"Batman Begins","Spider Man 2");
-		Manager mng1 = new Manager(1,"Tyler","pw2",55.00,"Jurassic Park","The Dark Knight");
-		
-		System.out.println(cust1.toString());
-		System.out.println(cust2.toString());
-		System.out.println(mng1.toString());
-		
-		cust1.purchase(mov1, "movie");
-		cust2.purchase(aud1,"book");
-	}
+    public static void main(String[] args)
+    {
+
+        Audiobook aud1 = new Audiobook("Josh", "2013", 120, "horror", 0, 30, 0,0);
+        Movie mov1 = new Movie("Josh", "2013", 120, "horror", 0, 30, 0,0);
+        
+        
+        Audiobook [] aInv = new Audiobook[1];
+        Movie [] mInv = new Movie[1];
+        mInv[0] = mov1;
+
+        DBIO.setMovieInventory(mInv);
+        DBIO.setBookInventory(aInv);
+
+        Customer cust1 = new Customer(1,"Josh","pw",25.00,"Transformers","Spider Man 3");
+        Customer cust2 = new Customer(2,"Jared","pw1",35.00,"Batman Begins","Spider Man 2");
+        Manager mng1 = new Manager(1,"Tyler","pw2",55.00,"Jurassic Park","The Dark Knight");
+
+        System.out.println(cust1.toString());
+        System.out.println(cust2.toString());
+        System.out.println(mng1.toString());
+
+        cust1.purchase(mov1, "movie");
+        cust2.purchase(aud1,"book");
+        
+        Movie mov3 = new Movie("Tyler", "Superbad", 60, "action", 3, 15, 0, 0);
+        
+        mng1.addMedia(mov3, "movie");
+        
+        System.out.println(DBIO.query("\nmovie"));
+        
+        System.out.println("\nThe movie " + mov3.name + " was sold " + Manager.getnumSold(mov3) + " times.");
+        
+        System.out.println("\n Retreive Customer information: \n" + Manager.getcustInfo(cust1));
+    }
 }
