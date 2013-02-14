810e45c74dea5181cb4121e9588fda6e308f6ab3
Jared Bean <jibean19@gmail.com>
2013-02-14 10:19:29 -0500

    Add comments

--- Store/src/store/StoreTest.java
+++ Store/src/store/StoreTest.java
@@ -3,9 +3,10 @@ public class StoreTest
     public static void main(String[] args)
     {
 
-        Audiobook aud1 = new Audiobook("Josh", "2013", 120, "horror", 0, 30, 0,0);
-        Movie mov1 = new Movie("Josh", "2013", 120, "horror", 0, 30, 0,0);
-        Movie[] tempArr;
+        Audiobook aud1 = new Audiobook("Josh", "2013", 120, "horror", 0, 30, 0,0); //start inventory
+        Movie mov1 = new Movie("Josh", "2013", 120, "horror", 0, 30, 0,0); //start inventory
+        Movie mov3 = new Movie("Tyler", "Superbad", 60, "action", 3, 15, 0, 0); //manager add
+        Movie[] tempArr; //temporary movie inventory array
         
         
         Audiobook [] aInv = new Audiobook[1];
@@ -19,17 +20,18 @@ public class StoreTest
         Customer cust2 = new Customer(2,"Jared","pw1",35.00,"Batman Begins","Spider Man 2");
         Manager mng1 = new Manager(1,"Tyler","pw2",55.00,"Jurassic Park","The Dark Knight");
 
+        //User info
         System.out.println(cust1.toString());
         System.out.println(cust2.toString());
         System.out.println(mng1.toString());
 
-        cust1.purchase(mov1, "movie");
-        cust2.purchase(aud1,"book");
+        cust1.purchase(mov1, "movie"); //Buys
+        cust2.purchase(aud1,"book"); //Not enough Credit!!
         
-        Movie mov3 = new Movie("Tyler", "Superbad", 60, "action", 3, 15, 0, 0);
-        
+       
         mng1.addMedia(mov3, "movie");
-        
+       
+        //Get and print new movie inventory
         tempArr = (Movie[])DBIO.query("movie");
         System.out.println(tempArr.length);
         for(int i=0; i<tempArr.length; i++){
