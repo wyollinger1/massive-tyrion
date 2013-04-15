0298706e9f6d6e0f9bb85557288714212b67aab9
Jared Bean <jibean19@gmail.com>
2013-04-15 17:22:48 -0400

    Make helper function fo doGet and doPost and add doDelete for logout

--- Store/src/servs/Login.java
+++ Store/src/servs/Login.java
@@ -28,29 +28,56 @@ public class Login extends HttpServlet {
 	public Login() {
 		super();
 
-		//Wrong need to use Servlet's Container's DB Connection pool
-		//TODO:Use connection pool
+		// Wrong need to use Servlet's Container's DB Connection pool
+		// TODO:Use connection pool
 		DBIO.init();
 		DBIO.setDb("/home/jbean/Code/221/massive-tyrion/Store/src/store/Store.sqlite");
 	}
 
 	/**
+	 * Same as doPost, logs on user
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
+	 * @see Login#logon(HttpServletRequest, HttpServletResponse)
 	 */
 	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		logon(request, response);
+	}
+
+	/**
+	 * Same as doGet, logs on user
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 * @see Login#logon(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		logon(request, response);
+	}
+
+	/**
+	 * Actual logon logic for servlets doPost and doGet
+	 * @param request the HttpServletRequest object
+	 * @param response the HttpServletResponse object
+	 * @throws ServletException
+	 * @throws IOException
+	 * @see Login#doPost(HttpServletRequest, HttpServletResponse)
+	 * @see Login#doGet(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void logon(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		Map<String, String[]> paramMap = request.getParameterMap();
 		User user = null;
 		HttpSession session = null;
 		boolean isManager = false;
 		boolean loggedIn = false;
-		
-		PrintWriter resWrite= response.getWriter();
-		// Going to return JSON so set header
-		response.setHeader("Content-Type", "application/json");
-		
 
+		PrintWriter resWrite = response.getWriter();
+		// Going to return JSON so set header and begin the list
+		response.setHeader("Content-Type", "application/json");
+		resWrite.write("[");
+		
 		// Login user
 		if (paramMap.containsKey("uname")
 				&& paramMap.get("uname")[0].length() > 0
@@ -67,29 +94,28 @@ public class Login extends HttpServlet {
 				session.setAttribute("user", user);
 				session.setAttribute("isManager", isManager);
 				session.setAttribute("loggedIn", loggedIn);
-				resWrite.write(String.format("[{\"name\":\"%s\","+
-				"\"balance\":%.2f, \"id\":%d}]", user.getName(), 
-				user.getBalance(), user.getID()));
-				resWrite.flush();
-				resWrite.close();
-				response.flushBuffer();
-				return;
+				resWrite.write(Util.userToJson(user));
+			}else{
+				resWrite.write("{\"Error\":\"Username and password mismatch\"}");
 			}
+		}else{
+			resWrite.write("{\"Error\":\"Invalid Parameters\"}");
 		}
-		resWrite.write("[]");
-		resWrite.flush();
-		resWrite.close();
-		response.flushBuffer();
 		
+		//Wrap it up
+		resWrite.write("]");
+		Util.closeRes(response, resWrite);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 * This be the logout just invalidates the session.
+	 * 
+	 * @see HttpServlet#doDelete(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request,
+	protected void doDelete(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		session.invalidate();
 	}
-
 }
