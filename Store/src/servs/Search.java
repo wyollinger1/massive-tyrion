3dad4ab87c45f16f6a441c09cef50d4b57df4136
Jared Bean <jibean19@gmail.com>
2013-04-15 02:02:03 -0400

    Add JSON response and DBIO search

--- Store/src/servs/Search.java
+++ Store/src/servs/Search.java
@@ -4,8 +4,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
 
 import store.*;
+import servs.Util;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,7 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Search
  */
-@WebServlet(description = "search", urlPatterns = { "/Search" })
+@WebServlet(description = "search", urlPatterns = { "/search" })
 public class Search extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -24,6 +28,8 @@ public class Search extends HttpServlet {
 	 * Default constructor.
 	 */
 	public Search() {
+		//Wrong need to use Servlet's Container's DB Connection pool
+		//TODO:Use connection pool
 		DBIO.init();
 		DBIO.setDb("/home/jbean/Code/221/massive-tyrion/Store/src/store/Store.sqlite");
 	}
@@ -34,6 +40,45 @@ public class Search extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
+		DBIO.Types mType=null;
+		DBIO.SearchField sField=null;
+		String searchStr="";
+		String json="[]";
+		ArrayList<Media> results=null;
+		Map <String, String[]> paramMap=request.getParameterMap();
+		
+		//Going to return JSON so set header
+		response.setHeader("Content-Type", "application/json");
+		
+		//Grab params if there
+		if(paramMap.containsKey("sField") && paramMap.containsKey("mType")){
+			if(paramMap.get("sField").length>0){
+				sField = Util.strToSearchField(paramMap.get("sField")[0]);
+			}
+			if(paramMap.get("mType").length>0){
+				mType = Util.strToType(paramMap.get("mType")[0]);
+			}
+			if(paramMap.containsKey("search") && paramMap.get("search").length>0){
+				searchStr=paramMap.get("search")[0];
+			}
+		}
+		//If I could get at least the mType and sField params
+		//and they were valid give a non-empty response
+		if(mType!=null && sField!=null){
+			json="[";
+			results=Customer.search(searchStr, sField, mType);
+			for(Iterator<Media> iter=results.iterator(); iter.hasNext(); ){
+				json+=Util.mediaToJson(iter.next());
+				json+=",";
+			}
+			json=json.substring(0, json.length()-1);
+			json+="]";
+			
+			
+		}
+		response.getWriter().write(json);
+		response.flushBuffer();
+		
 		
 	}
 
@@ -45,5 +90,7 @@ public class Search extends HttpServlet {
 			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
+	
+	
 
 }
